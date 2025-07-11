38e0020b60fc9eddddb9ea56129b4aa2c0fde527
Sila MEAS <sila.meas@allweb.com.kh>
2025-07-11 12:46:19 +0700

    kong volumes

--- restaurant/src/main/java/com/sila/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/com/sila/restaurant/RestaurantApplication.java
@@ -1,9 +1,11 @@
 package com.sila.restaurant;
 
+import com.silacode.config.EnableAuth0Security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAuth0Security
 public class RestaurantApplication {
 
   public static void main(String[] args) {
